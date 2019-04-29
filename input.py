60d3f98f4bf5112bfb43767fd04346c3cdf33a9a
kakioshe <louis.kakioshe@gmail.com>
2019-04-29 23:15:06 +1000

    temporary work

---
+++ input.py
@@ -0,0 +1,79 @@
+import RPi.GPIO as GPIO
+import time
+from ibm_watson import ToneAnalyzerV3
+import json
+import wave
+import pyaudio
+import speech_recognition as sr
+
+def record():
+    form_1 = pyaudio.paInt16
+    chans = 1
+    samp_rate = 44100
+    chunk = 4096
+    record_secs = 5
+    dev_index = 2
+    wav_output_filename = 'story.wav'
+    audio = pyaudio.PyAudio()
+    stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
+                    input_device_index = dev_index,input = True, \
+                    frames_per_buffer=chunk)
+    print("recording")
+    frames = []
+    for ii in range(0,int((samp_rate/chunk)*record_secs)):
+        data = stream.read(chunk)
+        frames.append(data)
+    print("finished recording")
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    wavefile = wave.open(wav_output_filename,'wb')
+    wavefile.setnchannels(chans)
+    wavefile.setsampwidth(audio.get_sample_size(form_1))
+    wavefile.setframerate(samp_rate)
+    wavefile.writeframes(b''.join(frames))
+    wavefile.close()
+    
+def speech_to_text(filename):
+    r = sr.Recognizer()
+    file = sr.AudioFile(filename)
+    with file as source:
+        audio = r.record(source)
+        text = r.recognize_google(audio)
+        print(text)
+        return text
+    
+def detect_emotions(text):
+    emotions = []
+    tone_analysis = tone_analyzer.tone({'text': text},content_type='application/json').get_result()
+    for i in tone_analysis['document_tone']['tones']:
+        emotions.append((i['tone_name']))  
+    return emotions
+
+tone_analyzer = ToneAnalyzerV3(
+    version='2017-09-21',
+    iam_apikey='BCPnn-DtaBVZEcIU75mDen9uvl8KqrCx7LBUGaIGZ2PQ',
+    url='https://gateway-syd.watsonplatform.net/tone-analyzer/api'
+)
+
+#state = False
+#counter = 2
+#while True:
+#    time.sleep(0.1)
+#    if (GPIO.input(26) == 1):
+#        for i in range(num_pixels):
+#            pixels[i] = (2*i,0,2*i)
+#            pixels.show()
+#        counter += 1
+#        record()
+#        detected = detect_emotions(speech_to_text("story.wav"))
+#        print(detected)
+#        #detected is emotion
+#        time.sleep(2)
+#    else:
+#        for i in range(num_pixels):
+#            pixels[i] = (0,2*i,2*i)
+#            pixels.show()
+
+
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 import time
+import RPi.GPIO as GPIO
+from input import record
+from fileDownload import *
 
 # SPEAKER SIDE
 # Status List:
@@ -11,39 +14,62 @@ import time
 # 4. feedback = When the listener has finished saying feedback, and its uploaded
 #               speaker side will play that feedback and some pre-recorded msg
 
+def updateStatus():
+    r = requests.get('http://the-untold.herokuapp.com/status')
+    return r.json()[0]['status']
+    
+
 def main():
     STATUS_LIST = ["idle", "speaker", "listener", "feedback"]
     HEROKU_URL = "http://the-untold.herokuapp.com/status/5cc1ab8dfb6fc0265f2903a3"
+    bucket = 'the-untold'
+    status = updateStatus()
+    
     while True:
         #Do infinite loop, catch current status each loop
-        r = requests.get('http://the-untold.herokuapp.com/status')
-        status = r.json()[0]['status']
         
-        if status == STATUS_LIST[1]:
-            print("SPEAKER Speaker currently speaking")
-            # DO THE RECORDING, and EMOTION DETECTOR CODE HERE
-            time.sleep(10)
-            emotion = "sad"
-            requests.put(HEROKU_URL, json={
-                "status": STATUS_LIST[2],
-                "emotion": emotion
-                })
+        if status != STATUS_LIST[0]:
             
-        elif status == STATUS_LIST[2]:
-            print("SPEAKER Listener currently making a feedback")
-            #DO play recorded msg
+            
+            status = updateStatus()
+            if status == STATUS_LIST[1]:
+                print("SPEAKER Speaker currently speaking")
+                # DO THE RECORDING, and EMOTION DETECTOR CODE HERE
+                time.sleep(10)
+                emotion = "sad"
+                requests.put(HEROKU_URL, json={
+                    "status": STATUS_LIST[2],
+                    "emotion": emotion
+                    })
+                
+            elif status == STATUS_LIST[2]:
+                print("SPEAKER Listener currently making a feedback")
+                time.sleep(2)
+                #DO play recorded msg
 
-        elif status == STATUS_LIST[3]:
-            print("SPEAKER Playing the feedback and other msgs")
-            filename = r.json()[0]['filename']
-            print("filename is {}".format(filename))
-            #DO play feedback and other recorded msg
-            time.sleep(10)
-            requests.put(HEROKU_URL, json={
-                "status": STATUS_LIST[0],
-                })
-
+            elif status == STATUS_LIST[3]:
+                print("SPEAKER Playing the feedback and other msgs")
+                r = requests.get('http://the-untold.herokuapp.com/status')
+                filename = r.json()[0]['filename']
+                emotion = r.json()[0]['emotion']
+                print("filename is {}".format(filename))
+                #DO play feedback and other recorded msg
+                fileDownload.downloadFile(bucket, emotion, filename)
+                requests.put(HEROKU_URL, json={
+                    "status": STATUS_LIST[0],
+                    })
         else:
-            print("idling")
+            if (GPIO.input(26) == 1):
+                print("button press")
+                requests.put(HEROKU_URL, json={
+                    "status": STATUS_LIST[1],
+                    })        
+                status = updateStatus()
+
+                
+            
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(26,GPIO.IN,pull_up_down = GPIO.PUD_DOWN)
 
 main()
